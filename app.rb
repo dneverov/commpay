aaca0104f360f3199b705bb2592b5a5992f725ce
Dennis Neverov <denis.neverov@gmail.com>
2020-02-04 03:08:40 +0300

    Create class Billing

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ Tariffs = {
   water_hot: 198.19,
   energy: 5.47,
   gas: 56.69,
-  phone: 0 #unused
+  phone: 1 #unused
 }
 
 # vars
@@ -13,28 +13,48 @@ Tariffs = {
   water_hot: 3, #536,  539, 542
   energy: 91, #5546, 5649, 5740
   gas: 1,
-  phone: 1
+  phone: 0
 }
 
 @previous_modifier = 6 #-39, -11
 
-def in_currency(value)
-  format("%.2f", value)
-end
 
-def calculate
-  total = 0
-  @values.each do |k,v|
-    calc = v * Tariffs[k]
-    puts "#{k}: #{in_currency calc}"
-    total += calc
+class Billing
+  attr_accessor :name, :modifier
+
+  def initialize(tariffs, values)
+    @name = "Unknown"
+    @modifier = 0
+    @tariffs = tariffs
+    @values = values
   end
 
-  puts "----------\nSubotal: #{ in_currency total }"
-  puts "Total:   #{ in_currency(total+@previous_modifier) }"
+  def calculate
+    puts "## #{name} ##"
+    total = 0
+    @values.each do |k,v|
+      calc = v * Tariffs[k]
+      puts "#{k}: #{in_currency calc}"
+      total += calc
+    end
+
+    puts "----------\nSubotal: #{ in_currency total }"
+    puts "Total:   #{ in_currency(total+modifier) }"
+  end
+
+  private
+
+    def in_currency(value)
+      format("%.2f", value)
+    end
 end
 
-calculate
+
+# The main App
+billing = Billing.new(@tariffs, @values)
+billing.name = "March 2020 Billing"
+billing.modifier = @previous_modifier
+billing.calculate
 
 #   January 2020
 # water_cold: 202.39999999999998
