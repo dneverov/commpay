d13cb720e294998dad2116c9e16d7bb0820951a3
Denis Neverov <denis.neverov@gmail.com>
2020-03-29 03:33:27 +0300

    Move Billing class to lib

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'lib/param'
+require_relative 'lib/billing'
 require_relative 'lib/billing_store'
 
 # Const
@@ -24,62 +25,6 @@ OutputWidth = 24
 @previous_modifier = 6 #-39, -11
 
 @@billing_params = []
-
-class Billing
-  attr_accessor :name, :modifier
-  attr_reader :subtotal, :total, :created_at
-
-  def initialize(tariffs, values)
-    @created_at = Time.now
-    @name = @created_at.strftime("%B %Y")
-    @modifier = 0
-    @tariffs = tariffs
-    @values = values
-  end
-
-  def calculate
-    puts "##{name.center(OutputWidth-2)}#"
-    puts hr
-    subtotal = 0
-    @values.each do |k,v|
-      calc = v * Tariffs[k]
-      puts formatted_float(k, calc)
-      subtotal += calc
-      # Do the same with Param
-      billing_param = Param.new(k, Tariffs[k])
-      billing_param.delta = v
-      billing_param.calculate
-      @@billing_params << billing_param
-    end
-
-    # Total
-    @subtotal = sprintf("%.2f", subtotal).to_f
-    @total = sprintf("%.2f", subtotal+modifier).to_f
-    # Render
-    puts hr
-    puts formatted_total("Subtotal", @subtotal)
-    puts formatted_total("Total", @total)
-  end
-
-  private
-
-    def hr(w=OutputWidth)
-      "-" * w
-    end
-
-    # TODO: remove (unused)
-    # def in_currency(value)
-    #   format("%.2f", value)
-    # end
-
-    def formatted_float(key, value, options = {:delimeter => "|"})
-      format(" %-11s #{options[:delimeter]} %8.2f", key, value)
-    end
-
-    def formatted_total(key, value)
-      formatted_float(key, value, delimeter: ' ')
-    end
-end
 
 # The main App
 billing = Billing.new(Tariffs, @values)
