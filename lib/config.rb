684a0c6cc1342c27605738fd35a58f99f4c8e106
Denis Neverov <denis.neverov@gmail.com>
2020-04-19 04:39:43 +0300

    Add localization (i18n)

--- lib/config.rb
+++ lib/config.rb
@@ -1,6 +1,8 @@
 # Configuration file
 #   Constants, Configs etc.
 
+require 'i18n'
+
 Tariffs = {
   water_cold: 40.48,
   water_hot: 198.19,
@@ -11,7 +13,11 @@ Tariffs = {
 
 CalculatedParameters = %w(water_cold water_hot energy)
 
-OutputWidth = 24
-
 # To hard save with defined entity_id. E.g. 'march_2020'. Set =nil to disable
 BindedId = nil
+
+I18n.load_path << Dir[File.expand_path("config/locales") + "/*.yml"]
+I18n.default_locale = :en # (note that `en` is already the default!)
+I18n.locale = :ru
+
+OutputWidth = I18n.t(:output_width, default: 24)
